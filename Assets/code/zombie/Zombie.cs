3ea426ce041ed9272e483ea6ce26fbaa8c078b11
IronPeak <HrafnOrri1207@gmail.com>
2016-03-18 14:20:51 +0000

    Zombie first increment

--- Assets/code/zombie/Zombie.cs
+++ Assets/code/zombie/Zombie.cs
@@ -1,18 +1,58 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class Zombie : MonoBehaviour
 {
+    public ZombieState path;
+    Queue<ZombieState> states = new Queue<ZombieState>();
 
-	void Start ()
+	void Update ()
     {
-        var Queue;
+        Queue<ZombieState> available = new Queue<ZombieState>();
         for(int i = 0; i < Graph.graph.graphNodes.Count; i++)
         {
             if(Node.CanReachNode(transform.position, Graph.graph.graphNodes[i]))
             {
-                Queue.enqueue(Graph.graph.graphNodes[i]);
+                available.Enqueue(new ZombieState(Graph.graph.graphNodes[i], Vector3.Distance(transform.position, Graph.graph.graphNodes[i].transform.position)));
+            }
+        }
+
+        uint count = 0;
+        while(available.Count != 0)
+        {
+            count++;
+            if(count > 10000)
+            {
+                return;
+            }
+            ZombieState next = available.Dequeue();
+            if(next.node.ReachPlayer)
+            {
+                path = next;
+                return;
+            }
+            for(int i = 0; i < next.node.adjNodes.Count; i++)
+            {
+                available.Enqueue(new ZombieState(next, next.node.adjNodes[i]));
             }
         }
 	}
+
+    void OnDrawGizmos()
+    {
+        if(path == null)
+            return;
+        if(Application.isPlaying == false)
+            return;
+        ZombieState n = path;
+        Gizmos.color = Color.black;
+        Gizmos.DrawLine(n.node.transform.position, Player.player.transform.position);
+        while(n.prev != null)
+        {
+            Gizmos.DrawLine(n.node.transform.position, n.prev.node.transform.position);
+            n = n.prev;
+        }
+        Gizmos.DrawLine(n.node.transform.position, transform.position);
+    }
 
 }
---
+++ Assets/code/zombie/ZombieState.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ZombieState : IComparer<ZombieState>
+{
+    public ZombieState prev;
+    public float distance;
+    public Node node;
+
+    public ZombieState(Node node, float distance)
+    {
+        prev = null;
+        this.distance = distance;
+        this.node = node;
+    }
+
+    public ZombieState(ZombieState prev, NodeConnection connection)
+    {
+        this.prev = prev;
+        this.distance = prev.distance + connection.distance;
+        this.node = connection.node;
+    }
+
+    public int Compare(ZombieState x, ZombieState y)
+    {
+        if(x.distance < y.distance)
+        {
+            return -1;
+        }
+        if(x.distance > y.distance)
+        {
+            return 1;
+        }
+        return 0;
+    }
+}
