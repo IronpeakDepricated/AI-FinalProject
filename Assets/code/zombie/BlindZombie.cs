348f064482579c1b38880dc7f7dd70075b79dcb1
Fanneyyy <fanneyyyy@gmail.com>
2016-03-20 16:10:27 +0000

    Created a very stubid patrol zombie that is also blind

---
+++ Assets/code/zombie/BlindZombie.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlindZombie : MonoBehaviour {
+	
+	// Update is called once per frame
+	bool forward = true;
+	float total = 0;
+	void Update () {
+		if (forward) {
+			transform.position += (new Vector3 (1, 0, 0)) * Time.deltaTime*5;
+		} else {
+			transform.position -= (new Vector3 (1, 0, 0)) * Time.deltaTime*5;
+		}
+		total += Time.deltaTime*5;
+		if (total >= 10) {
+			forward = !forward;
+			total = 0;
+		}
+	}
+}
