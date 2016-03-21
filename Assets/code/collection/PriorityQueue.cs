d4c8e96c81859aa44ce9ebc5bb3f36fa0cca1d6b
Drulludanni <gudni14@ru.is>
2016-03-21 18:11:45 +0000

    made log n PQ, hopefully working perfectly, but no guarantees

--- Assets/code/collection/PriorityQueue.cs
+++ Assets/code/collection/PriorityQueue.cs
@@ -1,8 +1,12 @@
-﻿public class PriorityQueue<T>
+﻿using System;
+
+public class PriorityQueue<T>
 {
+    // heavily borrowed from http://algs4.cs.princeton.edu/24pq/MaxPQ.java.html
+    // since c# has no built in PQ implementation
+
     class Node
     {
-        public Node next;
         private float weight;
         private T item;
 
@@ -22,22 +26,20 @@
             }
         }
 
-        public Node(float weight, T item, Node next)
+        public Node(float weight, T item)
         {
             this.weight = weight;
             this.item = item;
-            this.next = next;
         }
     }
 
     private int count;
-    private Node front, back;
+    private Node[] arr;
 
     public PriorityQueue()
     {
         count = 0;
-        front = null;
-        back = null;
+        arr = new Node[8];
     }
 
     public bool IsEmpty()
@@ -45,63 +47,76 @@
         return count == 0;
     }
 
-    public void Push(float weight, T item)
+    public int Size() {
+        return count;
+    }
+
+    private void resize(int capacity)
     {
-        if(IsEmpty())
+        if (capacity <= count)
         {
-            front = new Node(weight, item, null);
-            back = front;
-            count++;
+            throw new Exception("invalid resize");
         }
-        else
+        Node[] temp = new Node[capacity];
+        for (int i = 1; i <= count; i++)
         {
-            if(weight < front.Weight)
-            {
-                front = new Node(weight, item, front);
-                count++;
-            }
-            else if(weight >= back.Weight)
-            {
-                back.next = new Node(weight, item, null);
-                back = back.next;
-                count++;
-            }
-            else
-            {
-                Node temp = front;
-                while(temp.next.Weight <= weight)
-                {
-                    temp = temp.next;
-                }
-                temp.next = new Node(weight, item, temp.next);
-                count++;
-            }
+            temp[i] = arr[i];
         }
+        arr = temp;
+    }
+
+    public void Push(float key, T val) {
+        Node it = new Node(key, val);
+        if (count >= arr.Length - 1) resize(2 * arr.Length);
+        arr[++count] = it;
+        swim(count);
     }
 
     public T Pop()
     {
-        count--;
-        T ret = front.Item;
-        front = front.next;
-        return ret;
+        if (IsEmpty()) return default(T);
+        T max = arr[1].Item;
+        swap(1, count--);
+        sink(1);
+        arr[count + 1] = null; 
+        if ((count > 0) && (count == (arr.Length - 1) / 4)) resize(arr.Length / 2);
+        return max;
     }
 
     public T Peek()
     {
-        T ret = front.Item;
-        return ret;
+        return arr[1].Item;
+    }
+    
+
+    private void swim(int k) {
+        while (k > 1 && greater(k / 2, k))
+        {
+            swap(k, k / 2);
+            k = k / 2;
+        }
     }
 
-    public override string ToString()
+
+    private void sink(int k)
     {
-        string ret = "";
-        Node temp = front;
-        while(temp != null)
+        while (2 * k <= count)
         {
-            ret += temp.Weight + " ";
-            temp = temp.next;
+            int j = 2 * k;
+            if (j < count && greater(j, j + 1)) j++;
+            if (!greater(k, j)) break;
+            swap(k, j);
+            k = j;
         }
-        return ret;
+    }
+
+    private void swap(int i, int j) {
+        Node tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    private bool greater(int i, int j) {
+        return arr[i].Weight > arr[j].Weight;
     }
 }
