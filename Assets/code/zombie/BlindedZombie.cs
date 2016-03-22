afd60abf515da8922e505fd4f383de223aae3d82
Kristinn Þorri <kristinn.thorri@gmail.com>
2016-03-22 12:01:12 +0000

    merge fixes

--- Assets/code/zombie/BlindedZombie.cs
+++ Assets/code/zombie/BlindedZombie.cs
@@ -3,13 +3,47 @@ using System.Collections;
 
 public class BlindedZombie : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Update is called once per frame
+    int direction = 0;
+    float total = 0;
+    public ZombieState path;
+
+    void Update()
+    {
+        path = null;
+        if (Node.CanReach(transform.position, Player.player.transform.position))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Player.player.transform.position, Time.deltaTime * 10);
+            path = null;
+            return;
+        }
+        else
+        {
+            switch (direction)
+            {
+                case 0:
+                    transform.position += (new Vector3(1, 0, 0)) * Time.deltaTime * 5;
+                    break;
+                case 1:
+                    transform.position -= (new Vector3(1, 0, 0)) * Time.deltaTime * 5;
+                    break;
+                default:
+                    break;
+            }
+            total += Time.deltaTime * 5;
+            if (total >= 10)
+            {
+                if (direction == 1)
+                {
+                    direction = 0;
+                }
+                else if (direction == 0)
+                {
+                    direction = 1;
+                }
+                total = 0;
+            }
+        }
+        
+    }
 }
