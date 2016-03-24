c29ebc8dc7cdf55eb4faf62e1ae17d988635027f
kthorri <kristinn.thorri@gmail.com>
2016-03-24 13:16:32 +0000

    fixes on player movement

--- Assets/code/player/Player.cs
+++ Assets/code/player/Player.cs
@@ -18,45 +18,6 @@ public class Player : MonoBehaviour
 	}
 
 	void Update() {
-		// Rotate left
-		if (Input.GetKey(KeyCode.LeftArrow)) 
-		{
-            
-			transform.Rotate(0, -RotationSpeed, 0);
-		}
-		// Rotate right
-		if (Input.GetKey(KeyCode.RightArrow)) 
-		{
-			transform.Rotate(0, RotationSpeed, 0);
-		}
-        
-        /*
-		// Strafe left
-		if (Input.GetAxis(KeyCode.A)) 
-		{ 
-            rb.MovePosition(transform.position - transform.right * Time.deltaTime * MovementSpeed);
-			//transform.position -= new Vector3(1,0,0);
-		}
-		// Move forward
-		if (Input.GetKey(KeyCode.W)) 
-		{
-			//transform.position += transform.forward*moveSpeed;
-            rb.MovePosition(transform.position + transform.forward * Time.deltaTime * MovementSpeed);
-
-		} 
-		// Move backward
-		if (Input.GetKey(KeyCode.S)) 
-		{
-            rb.MovePosition(transform.position - transform.forward * Time.deltaTime * MovementSpeed);
-
-			//transform.position -= transform.forward * moveSpeed;
-		}
-		// Strafe right     
-		if (Input.GetKey(KeyCode.D)) 
-		{
-            rb.MovePosition(transform.position + transform.right * Time.deltaTime * MovementSpeed);
-			//transform.position += new Vector3(1,0,0);
-		} */
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis ("Vertical");
 
