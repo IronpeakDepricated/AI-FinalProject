99ecdf5c0b67a8dff2fed993b7d7d6e4f0678d68
kthorri <kristinn.thorri@gmail.com>
2016-03-24 14:28:35 +0000

    added shots to scene2

--- Assets/code/player/Player.cs
+++ Assets/code/player/Player.cs
@@ -8,6 +8,12 @@ public class Player : MonoBehaviour
     
     public float MovementSpeed;
     public float RotationSpeed;
+    
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    
+    private float nextFire;
 
 
 	void Awake ()
@@ -17,12 +23,22 @@ public class Player : MonoBehaviour
         
 	}
 
-	void Update() {
+	void Update() 
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+	}
+    
+    void FixedUpdate()
+    {
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis ("Vertical");
 
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
         rb.velocity = movement.normalized * MovementSpeed;
-	}
+    }
 
 }
